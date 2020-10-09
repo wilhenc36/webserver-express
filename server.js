d282378b5d132e68a63a0c823defbecfda2a4bcd
Wilhen Cruz <wilhencruz36@yahoo.com>
2020-10-09 00:59:37 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+require("./hbs/helpers");
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"));
+
+// Espress HBS engine
+hbs.registerPartials(__dirname + "/views/parciales")
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    nombre: "Wilhen",
+    anio: new Date().getFullYear()
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    anio: new Date().getFullYear()
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Escuchando peticiones en el puerto ${port}`);
+});
